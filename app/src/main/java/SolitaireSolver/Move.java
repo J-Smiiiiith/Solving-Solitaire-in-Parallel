340210822122fe2a7c68f9be55663f943bdccd28
James <jamesas1210@gmail.com>
2025-03-28 14:57:52 +0000

    Update Move class
    - When completing a move you do not need to know the source of the card. Only the destination is important.

--- app/src/main/java/SolitaireSolver/Move.java
+++ app/src/main/java/SolitaireSolver/Move.java
@@ -2,39 +2,17 @@ package SolitaireSolver;
 
 public class Move {
     Card card;
-    Pile src;
     Pile dst;
-    int heuristic, moveType;
+    int heuristic;
 
     public Move(Card card) {
-        //Stock to Foundation move
+        //Stock to Foundation move / Pile to Foundation move
         this.card = card;
-        this.moveType = 0;
     }
 
-    public Move(Card card, Pile src) {
-        //Pile to Foundation move
-        this.card = card;
-        this.src = src;
-        this.moveType = 1;
-    }
-
-    public Move(Card card, Pile src, Pile dst) {
-        //Pile to Pile move
-        this.card = card;
-        this.src = src;
-        this.dst = dst;
-        this.moveType = 3;
-    }
-
-    public Move(Card card, Pile dst, boolean isStockMove) {
-        //Stock to Pile move
+    public Move(Card card, Pile dst) {
+        //Pile to Pile move / Stock to Pile move
         this.card = card;
         this.dst = dst;
-        this.moveType = 4;
-    }
-
-    public int getMoveType() {
-        return moveType;
     }
 }
