a5ba449d3c56c15ed474b3bd23612ae8b44e2cfa
James <jamesas1210@gmail.com>
2025-04-04 14:46:19 +0100

    Create determineHeuristic method
    - Creates a method that adjusts the heuristic value of a move depending on the type of move it is.
    - This favours revealing cards, moving cards to the foundation etc.
    - This method is called once the move type has been determined

--- app/src/main/java/SolitaireSolver/Move.java
+++ app/src/main/java/SolitaireSolver/Move.java
@@ -9,17 +9,93 @@ public class Move {
     public Move(Card card) {
         //Stock to Foundation move / Pile to Foundation move
         this.card = card;
+        this.heuristic = 0;
     }
 
     public Move(Card card, Pile dst) {
         //Pile to Pile move / Stock to Pile move
         this.card = card;
         this.dst = dst;
+        this.heuristic = 0;
+    }
+
+    public void determineMoveType(Pile[] piles) {
+        if (dst == null) {
+            this.setMoveType(0);
+            // Stock to Foundation / Pile to Foundation
+        } else if (card.getLocation() == 7) {
+            this.setMoveType(1);
+            // Stock to Pile
+        } else if (piles[card.getLocation()].getBottomCard() == card) {
+            this.setMoveType(2);
+            // Pile to Pile: Move entire pile
+        } else {
+            this.setMoveType(3);
+            // Pile to Pile: Move partial pile
+        }
+        this.determineHeuristic(piles);
+    }
+
+    private void determineHeuristic(Pile[] piles) {
+        switch (moveType) {
+            case 0:
+                for (Pile pile : piles) {
+                    for (Card card : pile.getHiddenCards()) {
+                        if (card.isBlack() != this.getCard().isBlack()) {
+                            if (card.getRank() == this.getCard().getRank() - 1) {
+                                this.updateHeuristic(-5);
+                                break;
+                            }
+                        }
+                    }
+                } // Stock to foundation move may block a card of an opposite colour and rank-1 from being moved
+                if (card.getLocation() != 7) {
+                    if ((!piles[card.getLocation()].getHiddenCards().isEmpty()) &&
+                            (piles[card.getLocation()].getBuildStack().size() == 1)) {
+                        this.updateHeuristic(10);
+                    } // Pile to foundation move will reveal a hidden card
+                }
+                this.updateHeuristic(10);
+                break;
+            case 1:
+                this.updateHeuristic(5);
+                break;
+            case 2:
+                if (!piles[card.getLocation()].getHiddenCards().isEmpty()) {
+                    this.updateHeuristic(10);
+                } // Full build stack move will reveal a hidden card
+                else {
+                    this.updateHeuristic(5);
+                } // Full build stack move will create an empty pile
+
+                if ((card.getRank() == 13) && (dst.getPile().isEmpty())) {
+                    if (piles[card.getLocation()].getHiddenCards().isEmpty()) {
+                        this.setHeuristic(0);
+                    } // Moving a king to empty pile creating another empty pile achieves nothing
+                    else {
+                        this.updateHeuristic(5);
+                    } // Moving a King to an empty pile
+                }
+                break;
+            case 3:
+                Card nextCard = piles[card.getLocation()].getBottomCard();
+                if (nextCard.getRank() == card.getRank() + 1) {
+                    for (Pile pile : piles) {
+                        if (pile.getTopCard().getRank() == nextCard.getRank() + 1) {
+                            if (pile.getTopCard().isBlack() != nextCard.isBlack()) {
+                                this.updateHeuristic(5);
+                            } // Partial build stack move could reveal another card on the next turn.
+                        }
+                    }
+                } else {
+                    this.setHeuristic(0);
+                } // Partial build stack move may not reveal a hidden card so has no benefit
+        }
     }
 
     @Override
     public String toString() {
-        return card.toString() + " " + this.getMoveType();
+        return card.toString() + " " + this.getMoveType() + " " + this.getHeuristic();
     }
 
     public Pile getDst() {
@@ -37,19 +113,13 @@ public class Move {
         this.moveType = moveType;
     }
 
-    public void determineMoveType(Pile[] piles) {
-        if (dst == null) {
-            this.setMoveType(0);
-            // Stock to Foundation / Pile to Foundation
-        } else if (card.getLocation() == 7) {
-            this.setMoveType(1);
-            // Stock to Pile
-        } else if (piles[card.getLocation()].getBottomCard() == card) {
-            this.setMoveType(2);
-            // Pile to Pile: Move entire pile
-        } else {
-            this.setMoveType(3);
-            // Pile to Pile: Move partial pile
-        }
+    public int getHeuristic() {
+        return heuristic;
+    }
+    public void setHeuristic(int heuristic) {
+        this.heuristic = heuristic;
+    }
+    public void updateHeuristic(int heuristic) {
+        this.heuristic += heuristic;
     }
 }
